62996e6f191be19a72526779308c340fd979748e
Chung Leong <chernyshevsky@hotmail.com>
2023-02-13 22:42:51 +0100

    Improved error handling

--- src/parse.js
+++ src/parse.js
@@ -34,7 +34,8 @@ export async function *generateJSON(source, options = {}) {
   let keyStart = 0, keyEnd = 0;
   let openBracket = 0, closeBracket = 0, stack = [];
   let inString = false, escaped = false, syntaxError = false;
-  let leftover = null, openingSequence = [], prevResult = undefined;
+  let openingSequence = [], prevResult = undefined, charIndex = 0;
+  let leftover = new Uint8Array(0); 
   for await (let buffer of source) {
     let index = 0;
     if (leftover) {
@@ -46,120 +47,141 @@ export async function *generateJSON(source, options = {}) {
     }
     let closingSequence = null;
     let safeEndIndex = 0;
-    if (!syntaxError) {
-      while (index < buffer.length) {
-        const c = buffer[index];
-        if (inString) {
-          if (escaped) {
-            escaped = false;
-          } else {
-            if (c === DOUBLE_QUOTE) {
-              keyEnd = index + 1;
-              inString = false;
-            } else if (c === BACK_SLASH) {
-              escaped = true;
-            }
-          }
+    while (index < buffer.length) {
+      const c = buffer[index];
+      if (inString) {
+        if (escaped) {
+          escaped = false;
         } else {
-          let safeOffset = -1;
           if (c === DOUBLE_QUOTE) {
-            keyStart = index;
-            inString = true;
-          } else if (c === LEFT_CURLY || c === LEFT_SQUARE) {
-            let key;
-            if (openBracket === LEFT_CURLY) {
-              const keyBuffer = buffer.subarray(keyStart, keyEnd);
-              try {
-                key = JSON.parse(decoder.decode(keyBuffer));
-              } catch (err) {
-                syntaxError = true;
-                break;
-              }
-            } else if (openBracket === LEFT_SQUARE) {
-              key = '#';
-            } else {
-              key = 'root';
-            }
-            // save parent object context
-            stack.push({ allowPartial, openBracket, closeBracket });
-            // start new context
-            allowPartial = allowPartial?.[key] ?? allowPartial?.['*']; 
-            openBracket = c;
-            closeBracket = (c === LEFT_CURLY) ? RIGHT_CURLY : RIGHT_SQUARE;
-          } else if (c === RIGHT_CURLY || c === RIGHT_SQUARE) {
-            if (stack.length === 0) {
-              syntaxError = true;
-              break;
-            }
-            // restore parent object context
-            ({ allowPartial, openBracket, closeBracket } = stack.pop());
-            // safe to cleave JSON after close bracket
-            if (allowPartial) {
-              safeOffset = 1;
-            }
-          } else if (c === COMMA) {
-            if (stack.length === 0) {
-              syntaxError = true;
-              break;
-            }
-            // safe to cleave JSON before comma
-            if (allowPartial) {
-              safeOffset = 0;
-            }
-          }
-          if (safeOffset !== -1) {
-            safeEndIndex = index + safeOffset;
-            // construct closing sequence
-            closingSequence = [];
-            for (const c of [ ...stack.map(c => c.closeBracket), closeBracket ]) {
-              if (c) {
-                const cb = (c === RIGHT_CURLY) ? '}' : ']';
-                closingSequence.unshift(cb);
-              }
-            }
+            keyEnd = index + 1;
+            inString = false;
+          } else if (c === BACK_SLASH) {
+            escaped = true;
           }
         }
-        index++;
-      } 
-    }
-    if (safeEndIndex > 0 && !syntaxError) {
-      const strBuffer = buffer.subarray(0, safeEndIndex);
-      // remove any leading comma
-      const str = decoder.decode(strBuffer).replace(/^\s*,\s*/, '');
-      const start = openingSequence.join('');
-      const end = closingSequence.join('');
-      let result = JSON.parse(start + str + end);
-      if (prevResult) {
-        // merge result into previous result
-        result = merge(prevResult, result, openingSequence.length);
-      }
-      if (yieldClosingBrackets) {
-        yield [ result, end ];
       } else {
-        yield result;
-      }
-      prevResult = result;
-      // construct opening sequence for the next chunk
-      openingSequence = [];
-      for (const cb of closingSequence) {
-        let ob = (cb === '}') ? '{' : '[';
-        if (ob === '{' && openingSequence.length > 0) {
-          // assign first property to dummy key
-          ob += '"":';
+        let safeOffset = -1;
+        if (c === DOUBLE_QUOTE) {
+          keyStart = index;
+          inString = true;
+        } else if (c === LEFT_CURLY || c === LEFT_SQUARE) {
+          let key;
+          if (openBracket === LEFT_CURLY) {
+            const keyBuffer = buffer.subarray(keyStart, keyEnd);
+            try {
+              key = JSON.parse(decoder.decode(keyBuffer));
+            } catch (err) {
+              syntaxError = true;
+              break;
+            }
+          } else if (openBracket === LEFT_SQUARE) {
+            key = '#';
+          } else {
+            key = 'root';
+          }
+          // save parent object context
+          stack.push({ allowPartial, openBracket, closeBracket });
+          // start new context
+          allowPartial = allowPartial?.[key] ?? allowPartial?.['*']; 
+          openBracket = c;
+          closeBracket = (c === LEFT_CURLY) ? RIGHT_CURLY : RIGHT_SQUARE;
+        } else if (c === RIGHT_CURLY || c === RIGHT_SQUARE) {
+          if (stack.length === 0) {
+            syntaxError = true;
+            break;
+          }
+          // restore parent object context
+          ({ allowPartial, openBracket, closeBracket } = stack.pop());
+          // safe to cleave JSON after close bracket
+          if (allowPartial) {
+            safeOffset = 1;
+          }
+        } else if (c === COMMA) {
+          if (stack.length === 0) {
+            syntaxError = true;
+            break;
+          }
+          // safe to cleave JSON before comma
+          if (allowPartial) {
+            safeOffset = 0;
+          }
         }
-        openingSequence.unshift(ob);
+        if (safeOffset !== -1) {
+          safeEndIndex = index + safeOffset;
+          // construct closing sequence
+          closingSequence = [];
+          for (const c of [ ...stack.map(c => c.closeBracket), closeBracket ]) {
+            if (c) {
+              const cb = (c === RIGHT_CURLY) ? '}' : ']';
+              closingSequence.unshift(cb);
+            }
+          }
+        }
       }
-      leftover = buffer.subarray(safeEndIndex);
-      // adjust key indices if a key happens to be in the leftover
-      keyStart -= safeEndIndex;
-      keyEnd -= safeEndIndex;
-    } else {
+      index++;
+    } 
+    let processed = false;
+    if (safeEndIndex > 0 && !syntaxError) {
+      try {
+        const strBuffer = buffer.subarray(0, safeEndIndex);
+        const str = decoder.decode(strBuffer);
+        charIndex += str.length;
+        // remove any leading comma
+        const fragment = str.replace(/^\s*,\s*/, '');
+        const start = openingSequence.join('');
+        const end = closingSequence.join('');
+        let result = JSON.parse(start + fragment + end);
+        if (prevResult) {
+          // merge result into previous result
+          result = merge(prevResult, result, openingSequence.length);
+        }
+        if (yieldClosingBrackets) {
+          yield [ result, end ];
+        } else {
+          yield result;
+        }
+        prevResult = result;
+        // construct opening sequence for the next chunk
+        openingSequence = [];
+        for (const cb of closingSequence) {
+          let ob = (cb === '}') ? '{' : '[';
+          if (ob === '{' && openingSequence.length > 0) {
+            // assign first property to dummy key
+            ob += '"":';
+          }
+          openingSequence.unshift(ob);
+        }
+        leftover = buffer.subarray(safeEndIndex);
+        // adjust key indices if a key happens to be in the leftover
+        keyStart -= safeEndIndex;
+        keyEnd -= safeEndIndex;
+        processed = true;  
+      } catch (err) {
+        syntaxError = true;
+      }
+    } 
+    if (!processed) {
       leftover = buffer;
+      if (syntaxError) {
+        break;
+      }
     }
   }
-  if (prevResult === undefined && leftover) {
-    // no object was found
-    const text = decoder.decode(leftover);
+  // text remaining at the end
+  const text = decoder.decode(leftover);
+  console.log({ text, prevResult, openingSequence })
+  if (prevResult === undefined) {
+    // no object was found--probably a scalar
     yield JSON.parse(text);
+  } else if (text.trim() || openingSequence.length > 0) {
+    // not just whitespaces remaining or closing brackets are missing
+    // an error was encountered
+    const str = text.replace(/^\s*,\s*/, '');
+    // if there's no opening sequence, a proper object was parsed but garbage follows
+    const start = (openingSequence.length > 0) ? openingSequence.join('') : '{}';
+    // pad the text with spaces at the beginning to make position line up with original
+    const space = ' '.repeat(charIndex - start.length);
+    JSON.parse(space + start + str);  
   }
 }
