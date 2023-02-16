6596b364438e2bce7560ba091945b3722b66f5ff
Chung Leong <chernyshevsky@hotmail.com>
2023-02-16 05:03:13 +0100

    Some progress on WordPress example.

--- examples/wordpress/server/index.mjs
+++ examples/wordpress/server/index.mjs
@@ -1,9 +1,8 @@
 import Fastify from 'fastify';
-import Etag from '@fastify/etag';
 import Static from '@fastify/static';
 import Caching from '@fastify/caching';
-import Compression from '@fastify/compress';
 import CORS from '@fastify/cors';
+import { createJSONStream } from 'progressive-json/server';
 import { createPool } from 'mysql2';
 import { query } from './mysql-query.mjs';
 
@@ -11,44 +10,69 @@ const { pathname: buildPath } = new URL('../build', import.meta.url);
 
 (async () => {
   // create MySQL connection pool
-  const pool = mysql.createPool({
+  const pool = createPool({
     host: '172.129.0.4',
     user: 'wordpress',
-    password: 'password',
+    password: 'wordpress',
     database: 'wordpress',
     waitForConnections: true,
     connectionLimit: 10,
   });  
 
   const fastify = Fastify({ ignoreTrailingSlash: true, trustProxy: true });
-  // use automatic etag generation
-  await fastify.register(Etag);
   // use cache control
   await fastify.register(Caching, { privacy: 'public', expiresIn: 300 });
-  // use compression
-  await fastify.register(Compression);
   // allow CORS
   await fastify.register(CORS, { origin: true });
   // add sendFile() to reply object
   await fastify.register(Static, { root: buildPath });
   // register routes
   fastify.get('/api/posts', async (req, reply) => {
-    const { 
-      page = 1, 
-      per_page: perPage = 10,
-    } = req.query;
-    const mysql = query(pool);
-    const ids = await mysql.all`
-      SELECT id FROM wp_posts 
-      WHERE post_type = 'post'
-      ORDER BY post_date DESC 
-      LIMIT ${perPage} OFFSET ${(page - 1) * perPage}
-    `;
-    const authors = await mysql.all`
-      SELECT * FROM wp_users W
-    `;
+    const page = parseInt(req.query.page ?? '1');
+    const perPage = parseInt(req.query.per_page ?? '10');
+    const posts = loadPosts(pool, page, perPage);
+    return createJSONStream(posts, undefined, 2);
+  });
+  fastify.get('/api-alt/posts', async (req, reply) => {
+    const page = parseInt(req.query.page ?? '1');
+    const perPage = parseInt(req.query.per_page ?? '10');
+    const posts = loadPosts(pool, page, perPage);
+    let list = [];
+    for await (const post of posts ){
+      list.push(post);
+    }
+    return list;
   });
   // start listening for requests
-  await fastify.listen({ host: 'localhost', port: 8080 });
-  console.log(`Running test website at http://localhost:8080/`);
+  await fastify.listen({ host: 'localhost', port: 8081 });
 })();
+
+async function* loadPosts(connection, page, perPage) {
+  const mysql = query(connection);
+  const [ postIds, authorIds ] = await mysql.columns`
+    SELECT id, post_author 
+    FROM wp_posts 
+    WHERE post_type = 'post'
+    ORDER BY post_date DESC 
+    LIMIT ${perPage} OFFSET ${(page - 1) * perPage}
+  `;
+  const authors = await mysql.all`
+    SELECT * 
+    FROM wp_users 
+    WHERE id in (${authorIds})
+  `;
+  const rows = mysql`
+    SELECT *
+    FROM wp_posts
+    WHERE id in (${postIds})
+    ORDER BY FIELD(id, ${postIds})
+  `;
+  try {
+    for await (const row of rows) {
+      yield row;
+    } 
+  } catch (err) {
+    console.log(err);
+    yield err;
+  }
+}
--- examples/wordpress/server/mysql-query.mjs
+++ examples/wordpress/server/mysql-query.mjs
@@ -1,37 +1,14 @@
-export const rowFields = Symbol('fields');
-
 export function query(connection) {
-  async function* query(strings, ...args) {
+  function query(strings, ...args) {
     const tokens = [];
-    const params = [];
     for (const [ index, string ] of strings.entries()) {
       tokens.push(string);
       if (index < args.length) {
-        tokens.push('??');
-        params.push(args[index]);
+        tokens.push(connection.escape(args[index]));
       }
     }
     const sql = tokens.join('');
-    const queue = new RowQueue({
-      size: 5,
-      pause: () => connection.pause(),
-      resume: () => connection.resume(),
-    });
-
-    connection.query(sql, params)
-      .on('error', err => queue.throw(err))
-      .on('fields', fields => queue.describe(fields))
-      .on('result', row => queue.push(row))
-      .on('end', _ => queue.close());
-
-    for (;;) {
-      const row = await queue.pull();
-      if (!row) {
-        break;
-      }
-      row[rowFields] = queue.fields;
-      yield row;
-    }
+    return connection.query(sql).stream({ highWaterMark: 5 });
   }
   
   async function all(strings, ...args) {
@@ -77,76 +54,9 @@ export function query(connection) {
   }
 
   query.all = all;
+  query.columns = columns;
   query.one = one;
   query.value = query.id = value;
   query.values = query.ids = values;
   return query;
-}
-
-class RowQueue {
-  constructor({ size, pause, resume }) {
-    this.size = size;
-    this.pause = pause;
-    this.resume = resume;
-    this.items = [];
-    this.fields = null;    
-    this.promise = null;
-    this.resolve = null;
-    this.reject = null;
-    this.closed = false;
-    this.paused = false;
-  }
-
-  describe(fields) {
-    this.fields = fields;
-  }
-  
-  async pull() {
-    if (this.items.length === 0) {
-      if (!this.closed) {
-        if (!this.promise) {
-          this.promise = new Promise((resolve, reject) => {
-            this.resolve = resolve;
-            this.reject = reject;
-          });
-        }
-        if (this.paused) {
-          this.resume();
-        }
-        await promise;
-      }
-    }
-    return this.items.shift();
-  }
-
-  push(item) {
-    const count = this.items.push(item);
-    if (count >= this.size && !this.paused) {
-      this.pause();
-      this.paused = true;
-    }
-    this.signal();
-  }
-
-  close() {
-    this.closed = true;
-    this.signal();
-  }
-
-  throw(err) {
-    if (this.reject) {
-      this.reject(err);
-    } else if (!this.promise) {
-      this.promise = Promise.reject(err);
-    }
-  }
-
-  signal() {
-    if (this.resolve) {
-      this.resolve();
-      this.promise = null;
-      this.resolve = null;
-      this.reject = null;
-    }
-  }
 }
